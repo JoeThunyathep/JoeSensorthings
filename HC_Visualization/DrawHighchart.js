2a946690bcd2cfefe0defbcbfe8552731c4c3022
Joe Thunyathep Santhanavanich <thunyathep.s@outlook.com>
2018-05-18 21:20:30 +0200

    Update_Draw_TableFunction
    Update_Draw_TableFunction

--- HC_Visualization/DrawHighchart.js
+++ HC_Visualization/DrawHighchart.js
@@ -8,24 +8,25 @@ Object.size = function (obj) {
 };
 //get Fuel Level Chart for 22/11/2017
 var Base = "http://localhost:8080/STA_Test_Rossani/v1.0/Datastreams(";
-var datasize, resultJson, matrix;
+var datasize, resultJson, matrix, resultRaw;
 
 //Still not work well with different - month
-var GenSerieData = function (dayFr, dayTo, monthFr, monthTo) {
+var GenSerieData = function (dayFr, dayTo, monthFr, monthTo, DS) {
     var text = ")/Observations?$orderby=resultTime&$filter=day(resultTime)%20ge%20" + dayFr + "%20and%20day(resultTime)%20le%20" + dayTo + "%20and%20month(resultTime)%20ge%20" + monthFr +"%20and%20month(resultTime)%20le%20" + monthTo+ "&$select=result,resultTime"
-    var FullRequestText = Base + 1 + text
+    var FullRequestText = Base + DS + text
     var resultJs = getJSONRequestSTA(FullRequestText);
     return resultJs;
 }
 //Request with ISO still have a bug that the time period hh:mm:ss.ss is not working
-var GenSerieDataN = function (isoFr,isoTo) {
-    var text = ")/Observations?$orderby=resultTime&$filter=resultTime%20ge%20'" + isoFr + "'%20and%20resultTime%20le%20'" + isoTo + "'&$select=result,resultTime"
-    var FullRequestText = Base + 1 + text
+var GenSerieDataN = function (isoFr,isoTo, DS) {
+    var text = ")/Observations?$orderby=resultTime&$filter=resultTime%20ge%20" + isoFr + "%20and%20resultTime%20le%20" + isoTo + "&$select=result,resultTime"
+    var FullRequestText = Base + DS + text
     var resultJs = getJSONRequestSTA(FullRequestText);
     return resultJs;
 }
 var getJSONRequestSTA = function (FullRequestText) {
     resultJson = [];
+    resultRaw = [];
     matrix = [];
     $.getJSON(FullRequestText, function (data) {
         datasize = Object.size(data.value);
@@ -33,6 +34,7 @@ var getJSONRequestSTA = function (FullRequestText) {
             var temp = [Date.parse(data.value[i].resultTime), Math.round(parseFloat(data.value[i].result) * 100) / 100];
             resultJson.push(temp); //input the resultTime in Unix
             matrix.push(temp[1]);
+            resultRaw.push([data.value[i].resultTime, temp[1]]);
         }
     });
     console.log("Processing request from SensorThings API ...\n (URL : " + FullRequestText + " )");
@@ -42,7 +44,7 @@ var getJSONRequestSTA = function (FullRequestText) {
 
 
 
-var drawChart = function (Data_Main, dateValue, monthValue, yearValue) {
+var drawChart = function (Data_Main, isoFr, isoTo, DS) {
     //setTimeout(function cb() {
     //var nameTag = 'E-bike ' + ebikeID;
     console.log("Drawing Graph using Highchart.js ...");
@@ -54,7 +56,7 @@ var drawChart = function (Data_Main, dateValue, monthValue, yearValue) {
             text: 'Temperature (Outside), @Aula, HFT'
         },
         subtitle: {
-            text: 'Date :' + dateValue +'.'+ monthValue + '.' + yearValue
+            text: 'From :' + isoFr + '>> to :' + isoTo 
         },
         credits: {
             enabled: false
@@ -62,8 +64,9 @@ var drawChart = function (Data_Main, dateValue, monthValue, yearValue) {
         xAxis: {
             type: 'datetime',
             dateTimeLabelFormats: { // don't display the dummy year    
-                minute: '%H:%M',
-                hour: '%H:%M'
+            day : '%e %b',    
+            minute: '%H:%M',
+                hour: '%e %b \ %H:%M'
             },
             title: {
                 text: 'Time'
@@ -100,7 +103,7 @@ var drawChart = function (Data_Main, dateValue, monthValue, yearValue) {
         },
 
         series: [{
-            name: 'Data on ' + dateValue +'.'+ monthValue + '.' + yearValue,
+            name: 'Datastreams ID :' + DS,
             yAxis: 0,
             type: 'spline',
             // Define the data points. All series have a dummy year
